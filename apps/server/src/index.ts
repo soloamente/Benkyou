0b333681eab38614f802cd309b0b7122a1f2375c
unknown <contatta.av@gmail.com>
2025-11-20 18:25:57 +0100

    feat: first commit

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,22 +4,22 @@ import { cors } from "@elysiajs/cors";
 import { auth } from "@benkyou/auth";
 
 const app = new Elysia()
-	.use(
-		cors({
-			origin: process.env.CORS_ORIGIN || "",
-			methods: ["GET", "POST", "OPTIONS"],
-			allowedHeaders: ["Content-Type", "Authorization"],
-			credentials: true,
-		}),
-	)
-	.all("/api/auth/*", async (context) => {
-		const { request, status } = context;
-		if (["POST", "GET"].includes(request.method)) {
-			return auth.handler(request);
-		}
-		return status(405);
-	})
-	.get("/", () => "OK")
-	.listen(3000, () => {
-		console.log("Server is running on http://localhost:3000");
-	});
+  .use(
+    cors({
+      origin: process.env.CORS_ORIGIN || "",
+      methods: ["GET", "POST", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  )
+  .all("/api/auth/*", async (context) => {
+    const { request, status } = context;
+    if (["POST", "GET"].includes(request.method)) {
+      return auth.handler(request);
+    }
+    return status(405);
+  })
+  .get("/", () => "Hello Elysia")
+  .listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
